2a2f99c55eac70e402a13bf466760b4becc7468d
HNJSlater <henry.slater@gmail.com>
2013-04-28 14:03:43 +0100

    Code tidy and proper add and remove graphics

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import pygame, sys, math, random
 from pygame.locals import *
 import random
+from constants import *
 
-
-unit = 800/17
 
 def spawn_baddies(spawn_points, baddies):
 	for point in spawn_points:
@@ -20,7 +19,6 @@ class Missile():
 	def paint(self, win):
 		pygame.draw.circle(win, pygame.Color(255,255,255), (self.x, self.y) , 5, 0)
 	def tick(self):
-		global unit
 		if self.x > 2000 or self.y > 2000:
 			self.alive = False
 
@@ -29,8 +27,8 @@ class Missile():
 			self.alive = False
 			return
 
-		bx = self.baddie.x + (float(unit)/2)
-		by = self.baddie.y + (float(unit)/2)
+		bx = self.baddie.x + (float(UNIT)/2)
+		by = self.baddie.y + (float(UNIT)/2)
 		dx = float(bx - self.x)
 		dy = float(by - self.y)
 		sx = 1 if dx > 0 else -1
@@ -43,7 +41,7 @@ class Missile():
 		else:
 			self.y += sy * int(5 * abs(dy/dx))
 			self.x += sx * int(5 * abs(dx/dy))
-		if (self.x >= self.baddie.x) and (self.x <= (self.baddie.x + unit)) and (self.y >= self.baddie.y) and (self.y <= (self.baddie.y + unit)):
+		if (self.x >= self.baddie.x) and (self.x <= (self.baddie.x + UNIT)) and (self.y >= self.baddie.y) and (self.y <= (self.baddie.y + UNIT)):
 			self.baddie.hit(10)
 			self.alive = False
 		
@@ -55,13 +53,15 @@ class Tower():
 		self.y = y
 		self.tick_count = 0
 
-		
+        @staticmethod
+        def static_paint(win,x,y):
+		pygame.draw.rect(win, pygame.Color(32,32,32), (x*UNIT, y*UNIT, UNIT, UNIT))
+
 	def paint(self, win):	
-		global unit
-		pygame.draw.rect(win, pygame.Color(32,32,32), (self.x*unit, self.y*unit, unit, unit))
+                Tower.static_paint(win, self.x, self.y)
+
 
 	def tick(self, baddies):
-		global unit
 		self.tick_count += 1
 
 		if self.tick_count > 20 and len(baddies) > 0:
@@ -69,13 +69,13 @@ class Tower():
 			closest = None
 			distance = 0 
 			for baddie in baddies:
-				new_distance = ((self.x*unit - baddie.x) ** 2) + ((self.y*unit - baddie.y) ** 2)
+				new_distance = ((self.x*UNIT - baddie.x) ** 2) + ((self.y*UNIT - baddie.y) ** 2)
 				if new_distance < distance or distance == 0:
 					closest = baddie
 					distance = new_distance
                         if distance < ((5 * 20) ** 2):
                             self.tick_count = 0
-                            return [Missile(self.x*unit+unit/2, self.y*unit+unit/2, closest)]
+                            return [Missile(self.x*UNIT+UNIT/2, self.y*UNIT+UNIT/2, closest)]
 
                 return []
 
@@ -125,7 +125,6 @@ class Baddie():
 			self.next_x = chosen_option[0]
 			self.next_y = chosen_option[1]
 	def tick(self, paths, grid):
-		global unit
 		if (self.mov == 10):
 			self.current_x = self.next_x
 			self.current_y = self.next_y
@@ -135,7 +134,7 @@ class Baddie():
 		else:
 			self.mov += 1
 	def paint(self,win):
-		pygame.draw.rect(win, pygame.Color(128,128,128), (self.x, self.y, unit, unit))
+		pygame.draw.rect(win, pygame.Color(128,128,128), (self.x, self.y, UNIT, UNIT))
 
 	def kill(self):
 		self.alive = False
@@ -146,13 +145,11 @@ class Baddie():
 	
 	@property
 	def x(self):
-                global unit
-		return (self.current_x + ((self.mov /10.0) * (self.next_x - self.current_x))) * unit
+		return (self.current_x + ((self.mov /10.0) * (self.next_x - self.current_x))) * UNIT
 	
 	@property
 	def y(self):
-                global unit
-		return (self.current_y + ((self.mov /10.0) * (self.next_y - self.current_y))) * unit
+		return (self.current_y + ((self.mov /10.0) * (self.next_y - self.current_y))) * UNIT
 		
 
 
@@ -214,7 +211,7 @@ def main():
 
 	fpsClock = pygame.time.Clock()
 
-	win = pygame.display.set_mode((800,800))
+	win = pygame.display.set_mode((SCREEN_SIZE,SCREEN_SIZE))
 	pygame.display.set_caption('game')
 
 	font = pygame.font.Font(None, 20)
@@ -233,27 +230,32 @@ def main():
 			grid[x,y] = 0;
 
 	grid[finish] = 1
+        paths = update_paths(finish,grid)	
 	spawn_baddies(spawn_points, baddies)
 
 	while True:
 		win.fill(pygame.Color(0,0,0))
 		ticks += 1	
 
+                pos = pygame.mouse.get_pos()
+                x = pos[0] / UNIT
+                y = pos[1] / UNIT
 		for event in pygame.event.get():
 			if event.type == QUIT:
 				pygame.quit()
 				sys.exit()
 			elif event.type == pygame.MOUSEBUTTONUP:
-				pos = pygame.mouse.get_pos()
-				x = pos[0] / unit
-				y = pos[1] / unit
+
+
 				if not (x,y) in spawn_points:
 					if grid[x,y] == 2:
 						grid[x,y] = 0
 						del towers[x,y]
-					else:	
+                                                paths = update_paths(finish,grid)	
+					elif len(towers) < MAX_TOWERS:	
 						grid[x,y] = 2
 						towers[x,y] = Tower(x,y)
+                                                paths = update_paths(finish,grid)	
 
 		keys = pygame.key.get_pressed()
 
@@ -265,7 +267,7 @@ def main():
 #			missiles.append(m)
 #
 
-		paths = update_paths(finish,grid)	
+
 		#for node in paths:
 		#	pygame.draw.rect(win, pygame.Color(min(255,int(8*paths[node])),0,0), (node[0]*unit, node[1]*unit, unit, unit))
 
@@ -286,9 +288,9 @@ def main():
 		if ticks > 100:
 			spawn_baddies(spawn_points, baddies)
 		
-                pygame.draw.rect(win, pygame.Color(255,255,255), (finish[0]*unit, finish[1]*unit, unit, unit))
+                pygame.draw.rect(win, pygame.Color(255,255,255), (finish[0]*UNIT, finish[1]*UNIT, UNIT, UNIT))
                 if survived > 0:
-                    pygame.draw.rect(win, pygame.Color(0,0,0),  (finish[0]*unit+1, finish[1]*unit+1, unit-2, survived*(unit-2)/16))
+                    pygame.draw.rect(win, pygame.Color(0,0,0),  (finish[0]*UNIT+1, finish[1]*UNIT+1, UNIT-2, survived*(UNIT-2)/16))
 				
 
 		for missile in missiles:
@@ -304,8 +306,17 @@ def main():
                 if survived >= 16:
                     sys.exit()
 
+                if len(towers) < MAX_TOWERS and (x,y) in grid and grid[x,y] == 0:
+                    Tower.static_paint(win,x,y)
+                elif (x,y) in grid and grid[x,y] == 2:
+                    dx = 5
+                    points = [ (x*UNIT-5, y*UNIT), (x*UNIT,y*UNIT-5), ((x+1)*UNIT+5, (y+1)*UNIT), ((x+1)*UNIT, (y+1)*UNIT+5) ]
+                    pygame.draw.polygon(win, CROSS_COLOR, points); 
+                    points = [ ((x+1)*UNIT, y*UNIT-5), ((x+1)*UNIT+5, y*UNIT), (x*UNIT, (y+1)*UNIT+5), (x*UNIT-5, (y+1)*UNIT) ]    
+                    pygame.draw.polygon(win, CROSS_COLOR, points); 
+
 		pygame.display.update()
-		fpsClock.tick(60)
+		fpsClock.tick(30)
 
 
 
